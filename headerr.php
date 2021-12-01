9adb391d0435db92dce8eef74b9be83c5047633a
Noel R Loera J <91570298+jaramillo041104@users.noreply.github.com>
2021-12-01 12:07:00 -0700

    Add files via upload

---
+++ headerr.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <title>20 Noel Loera </title>
+    
+	<link rel="stylesheet" href="/proyecto_Loera_RegistroCivilF/estilos.css">
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<img src="/proyecto_Loera_RegistroCivilF/img/logo.png"> 
+			</div>
+			<nav>
+				<ul>
+					<li><a href="/proyecto_Loera_RegistroCivilF/index.php">Inicio</a></li>
+					<li><a href="/proyecto_Loera_RegistroCivilF/crud_empleado/empleado.php">Empleado</a></li>
+					<li><a href="/proyecto_Loera_RegistroCivilF/crud_bodas/bodas.php">Novio </a></li>
+					<li><a href="/proyecto_Loera_RegistroCivilF/crud_novia/novia.php">Novia </a></li>
+					<li><a href="/proyecto_Loera_RegistroCivilF/crud_actas/bodas.php">Acta de Nacimiento </a></li>
+
+				</ul>
+			</nav>
+		</div>
+	</header>
